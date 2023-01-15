a984d9bf57becb3513a611817f116b396e29fa05
OleksandrKhlopenko <108742474+OleksandrKhlopenko@users.noreply.github.com>
2023-01-15 18:00:45 +0200

    work_27

---
+++ script/script.js
@@ -0,0 +1,11 @@
+"use strict"
+function unique(arr) {
+    let set =new Set(values) 
+   
+   set.forEach(value=> alert(value))
+   
+}
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+"Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+unique();
